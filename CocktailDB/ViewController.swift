0d15d328ab523075d94c1659d544752604700c0c
VeronikaBabii <veronika.babii.2019@gmail.com>
2020-07-13 15:19:53 +0300

    Initial Commit

---
+++ CocktailDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CocktailDB
+//
+//  Created by Veronika Babii on 13.07.2020.
+//  Copyright © 2020 Veronika Babii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
